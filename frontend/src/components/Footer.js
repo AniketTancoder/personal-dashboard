4d49ce54e2af6cedb06cc7ee2b73271015bbb0eb
AniketTancoder <aniketpatil192847@gmail.com>
2025-09-21 15:38:01 +0530

    fix: footer is changed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,15 +5,16 @@ const Footer = () => {
   return (
     <footer className="app-footer">
       <div className="footer-content">
+        <div className="footer-links">
+            <span className="footer-status">Status: Operational</span>
+            <span className="footer-version">Version: 1.0.1</span> {/* Changed from 1.0.0 to 1.0.1 */}
+        </div>
         <p>
           🚀 Built by Aniket Patil | 
           deployed via CI/CD to AWS EKS | 
           {new Date().getFullYear()}
         </p>
-        <div className="footer-links">
-            <span className="footer-status">Status: Operational</span>
-            <span className="footer-version">Version: 1.0.1</span> {/* Changed from 1.0.0 to 1.0.1 */}
-        </div>
+        
       </div>
     </footer>
   );
